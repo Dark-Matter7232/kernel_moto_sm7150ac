80a0b5dc1adfe8cce8c260ba82b6b76030138f67
Alexander Winkowski <dereference23@outlook.com>
2023-06-11 16:16:25 +0000

    drm/msm: Optimize IRQ handlers
    On this kernel, `kms` cannot be NULL.
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1058,7 +1058,6 @@ static irqreturn_t msm_irq(int irq, void *arg)
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
 
-	BUG_ON(!kms);
 	return kms->funcs->irq(kms);
 }
 
@@ -1067,7 +1066,6 @@ static void msm_irq_preinstall(struct drm_device *dev)
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
 
-	BUG_ON(!kms);
 	kms->funcs->irq_preinstall(kms);
 }
 
@@ -1076,7 +1074,6 @@ static int msm_irq_postinstall(struct drm_device *dev)
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
 
-	BUG_ON(!kms);
 	return kms->funcs->irq_postinstall(kms);
 }
 
@@ -1085,7 +1082,6 @@ static void msm_irq_uninstall(struct drm_device *dev)
 	struct msm_drm_private *priv = dev->dev_private;
 	struct msm_kms *kms = priv->kms;
 
-	BUG_ON(!kms);
 	kms->funcs->irq_uninstall(kms);
 }
 
